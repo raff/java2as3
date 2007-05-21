564e36320371539c9d91fc2ba75417e744682ec0
raff <raff>
2007-05-21 16:40:45 +0000

    A test for ByteArray

---
+++ tested/ByteArray.java
@@ -0,0 +1,14 @@
+
+class ByteArray
+{
+  public byte[] byteArray;
+
+  ByteArray()
+  {
+    byteArray = new byte[50];
+    
+    byte[] singleByte = { 1 };
+
+    byte[] more = { 1, 2, 3, 4, 5 };
+  }
+}
