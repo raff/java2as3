da4ee08fcbacd147997d169bb59a886ac779c83a
raff <raff>
2007-07-03 15:53:56 +0000

    more test cases

--- tested/Another.java
+++ tested/Another.java
@@ -2,10 +2,24 @@ package test;
 
 public abstract class Another extends java.util.HashMap
 {
+	public abstract void abstractMethod();
+
 	public void something()
 	{
+/*
 		Object x = new Object() {
 			public String toString() { return "aString"; }
 		};
+*/
+	}
+
+	public void something(String elze)
+	{
+	}
+
+	public void hello()
+	{
+		something();
+		something("else");
 	}
 }
