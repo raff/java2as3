237fc5877feafd437dfaea8c5d43fd518a5d4361
raff <raff>
2008-10-22 14:43:30 +0000

    FB3 changed paths to SDK folders

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ SYSTEM=`uname`
 if [ `expr ${SYSTEM} : "^CYG"` -ne 0 ]; then
   ASC="`cygpath -d -m 'c:/Program Files/Adobe/Flex Builder 3/sdks/3.0.0/bin/compc.exe'`"
 else
-  ASC="/Applications/Adobe Flex Builder 3/Flex SDK 3/bin/compc"
+  ASC="/Applications/Adobe Flex Builder 3/sdks/3.0.0/bin/compc"
 fi
 
 #cp -r jas as3
